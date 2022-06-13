133cd473a3b04a9d9f44059ea398d58d11fd5359
jboxxx <jdealtrey@gmail.com>
2022-06-12 21:42:31 -0400

    cleanup

--- src/middlewares/checkSignature.js
+++ src/middlewares/checkSignature.js
@@ -1,27 +1,22 @@
 import decrypt from '../util/crypto';
 
 const checkSignature = (req, res, next) => {
-    try {
-        console.log('req.headers', req.headers);
+    if (
+        req.headers?.ss_access_token_iv &&
+        req.headers?.ss_access_token_content
+    ) {
         if (
-            req.headers?.ss_access_token_iv &&
-            req.headers?.ss_access_token_content
+            decrypt({
+                iv: req.headers.ss_access_token_iv,
+                content: req.headers.ss_access_token_content,
+            }) === process.env.SECRET_MESSAGE
         ) {
-            if (
-                decrypt({
-                    iv: req.headers.ss_access_token_iv,
-                    content: req.headers.ss_access_token_content,
-                }) === process.env.SECRET_MESSAGE
-            ) {
-                // authenticated
-                next();
-            }
+            // authenticated
+            next();
         }
-        // unauthenticated
-        res.sendStatus(403);
-    } catch (err) {
-        next(err);
     }
+    // unauthenticated
+    res.sendStatus(403);
 };
 
 export default checkSignature;
