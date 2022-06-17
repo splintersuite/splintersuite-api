d8c7899d0df522a480f618b0f52ed207115c5f2e
Logan Bernard <bernard.logan4@gmail.com>
2022-06-16 22:37:00 -0400

    in progress invoice

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,12 +8,31 @@ const router = express.Router();
 router.get('/:username', async (req, res) => {
     const { username } = req.params;
 
-    const [user] = await Users.query().where({ username });
+    let [user] = await Users.query().where({ username });
+
+    console.log('USER', user);
+    console.log('-------------------------------------------');
+
+    if (!user?.id) {
+        const now = new Date();
+        const users = await Users.query()
+            .insert({
+                username,
+                created_at: now,
+            })
+            .returning('*');
+        console.log(users);
+        user = users[0];
+    }
+
+    console.log('USER 2', user);
+    console.log('-------------------------------------------');
+
     const invoices = await Invoices.query()
         .join('seasons', { 'invoices.season_id': 'seasons.id' })
         .select('invoices.*', 'seasons.name')
         .where({ user_id: user.id });
-    user.invoices = invoices;
+    user.invoices = invoices || [];
 
     res.send(user);
 });
