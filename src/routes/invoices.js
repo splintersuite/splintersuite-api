b4ee603d101dcdddf3bcc84cabb87611b3e86724
jboxxx <jdealtrey@gmail.com>
2022-06-18 18:09:23 -0400

    invoices endpoint

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import { payInvoice, createInvoice } from '../controllers/Invoices';
+import {
+    payInvoice,
+    getInvoices,
+    createInvoice,
+} from '../controllers/Invoices';
 
 const router = express.Router();
 
 router.post('/:id', payInvoice);
+
+router.post('/invoices', getInvoices);
 
 router.get('/:username', createInvoice);
 
