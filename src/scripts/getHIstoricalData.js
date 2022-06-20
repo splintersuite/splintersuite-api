5beac41185b9b346800119a0b515464d269c665a
hackinhukk <trevorthomas@mac.com>
2022-06-20 06:31:00 -0400

    added cardDetails.json file, realized need to circle back on historicalData and add insert

---
+++ src/scripts/getHIstoricalData.js
@@ -0,0 +1,36 @@
+const histFncs = require('../services/marketData/historicalData');
+const cardsDetails = require('../util/cardDetails.json');
+const retryFncs = require('../services/axios_retry/general');
+const logger = require('../util/pinologger');
+
+// TNT TODO: actually insert the data into our db
+const getHistoricalData = async () => {
+    try {
+        logger.debug('getHistoricalData start');
+        // runs at 05:00 EST
+        // runs at 17:00 EST
+        const now = new Date();
+        const twelveHoursAgo = new Date(now.getTime() - 12 * 60 * 60 * 1000);
+        const twelveHoursAgoTime = twelveHoursAgo.getTime();
+
+        const fiveMinutesInMS = 1000 * 60 * 5;
+        let count = 0;
+        for (const card of cardsDetails) {
+            await histFncs.collectData({
+                card,
+                now,
+                twelveHoursAgo,
+                twelveHoursAgoTime,
+            });
+            if (count !== 0 && count % 100 === 0) {
+                await retryFncs.sleep(fiveMinutesInMS);
+            }
+            count++;
+        }
+        logger.debug('getHistoricalData done');
+        return;
+    } catch (err) {
+        logger.error(`getHistoricalData error: ${err.message}`);
+        throw err;
+    }
+};
