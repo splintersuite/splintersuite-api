a8b6f952eeda899845b4a16a935bf451a774aeb4
jboxxx <jdealtrey@gmail.com>
2022-07-17 15:28:59 -0400

    hive get transaction

---
+++ src/services/hive.js
@@ -0,0 +1,36 @@
+const { Client } = require('@hiveio/dhive');
+
+const client = new Client([
+    'https://api.hive.blog',
+    'https://api.hivekings.com',
+    'https://anyx.io',
+    'https://api.openhive.network',
+]);
+
+const getHiveTransaction = async ({ transactionId }) => {
+    try {
+        const data = await client.database.getTransaction(transactionId);
+        if (
+            Array.isArray(data?.operations) &&
+            data.operations.length === 1 &&
+            Array.isArray(data.operations[0]) &&
+            data.operations[0].length > 1 &&
+            data.operations[0][1]['json'] !== undefined
+        ) {
+            return JSON.parse(data.operations[0][1].json);
+        }
+        return null;
+    } catch (err) {
+        if (err.jse_info.code === 10) {
+            return null;
+        }
+        throw err;
+    }
+};
+
+// getHiveTransaction({
+//     transactionId: 'ddaaa1aae91334f8001383b5ab6f3a1593c7f37d',
+// });
+module.exprts = {
+    getHiveTransaction,
+};
