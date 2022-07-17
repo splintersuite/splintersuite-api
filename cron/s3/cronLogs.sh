9a5d7dd59678e0637b9d6f686926f52f606be86e
hackinhukk <trevorthomas@mac.com>
2022-07-17 12:55:44 -0400

    should work

--- cron/s3/cronLogs.sh
+++ cron/s3/cronLogs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -fux
-LOG_FILE="/home/ubuntu/cronLogs"
+LOG_FILE="/home/ubuntu/cronLogs/"
 NOW=$(date +%m-%d-%Y)
 S3_PATH="s3://terraform-splintersuite-api-logs-staging/cronLogs/$NOW"
 
@@ -9,6 +9,12 @@ export AWS_PROFILE=splintersuite-api-staging
 
 cd $LOG_FILE || "unable to cd into log file" 
 
-aws s3 cp $LOG_FILE/ $S3_PATH --exclude "*" --include "*.log"
+aws s3 cp $LOG_FILE $S3_PATH --recursive --exclude "*" --include "*.log" --exclude "*/*" --debug
 
 exit 0;
+
+# aws s3 cp . s3://terraform-splintersuite-api-logs-staging/cronLogs --recursive --exclude "*" --include "*.log" --exclude "*/*"
+
+
+# script that works manually in CLI: 
+# aws s3 cp /home/ubuntu/cronLogs/ s3://terraform-splintersuite-api-logs-staging/cronLogs --recursive --exclude "*" --include "*.log" --exclude "*/*" --debug
