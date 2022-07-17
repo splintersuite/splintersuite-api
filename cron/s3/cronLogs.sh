590b5d5a2a35fda9de3ce4262b0b6604e74906de
hackinhukk <trevorthomas@mac.com>
2022-07-17 12:40:23 -0400

    trying to debug

--- cron/s3/cronLogs.sh
+++ cron/s3/cronLogs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-
+set -fux
 LOG_FILE="/home/ubuntu/cronLogs"
 NOW=$(date +%m-%d-%Y)
 S3_PATH="s3://terraform-splintersuite-api-logs-staging/cronLogs/$NOW"
 
 export AWS_PROFILE=splintersuite-api-staging
 
-cd $LOG_FILE || exit 
+cd $LOG_FILE || "unable to cd into log file" 
 
-aws s3 cp $LOG_FILE/ $S3_PATH --exclude "*" --include "*.log" || exit
+aws s3 cp $LOG_FILE/ $S3_PATH --exclude "*" --include "*.log"
 
 exit 0;
