479ebba4a4fe081adaee325157edcad7b4361dd7
hackinhukk <trevorthomas@mac.com>
2022-07-17 16:33:23 -0400

    added more bash scripts, including for s3 backups

--- cron/s3/cronLogs.sh
+++ cron/s3/cronLogs.sh
@@ -9,12 +9,9 @@ export AWS_PROFILE=splintersuite-api-staging
 
 cd $LOG_FILE || "unable to cd into log file" 
 
-aws s3 cp $LOG_FILE $S3_PATH --recursive --exclude "*" --include "*.log" --exclude "*/*" --debug
+aws s3 cp $LOG_FILE $S3_PATH --recursive --exclude "*" --include "*.log" --exclude "*/*"
 
 exit 0;
-
-# aws s3 cp . s3://terraform-splintersuite-api-logs-staging/cronLogs --recursive --exclude "*" --include "*.log" --exclude "*/*"
-
 
 # script that works manually in CLI: 
 # aws s3 cp /home/ubuntu/cronLogs/ s3://terraform-splintersuite-api-logs-staging/cronLogs --recursive --exclude "*" --include "*.log" --exclude "*/*" --debug
