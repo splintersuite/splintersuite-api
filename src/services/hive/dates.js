b1d1595f0e36bfc34f4057d67319db525f6ea008
hackinhukk <trevorthomas@mac.com>
2023-01-14 04:34:20 -0500

    hive_tx_date insert is working properly at least on initial ones, havent tested with new data yet

--- src/services/hive/dates.js
+++ src/services/hive/dates.js
@@ -42,34 +42,70 @@ const updateHiveTxDates = async () => {
         throw err;
     }
 };
+// TNT NOTE: this is the start of if we ever have to update the created_ats, but seems like we shoudl just normally upload them with the dates imo, they're useless without the dates
+// const updateHiveTxsWithNoDates = async () => {
+//     try {
+//         logger.debug(`/services/hive/dates/updateHiveTxsWithNoDates`);
+
+//         const nullCreatedAts = await HiveTxDate.query()
+//             .select('hive_tx_id')
+//             .whereNull('hive_created_at');
+//         const otherNullCreatedAts = await HiveTxDate.query()
+//             .select('hive_tx_id')
+//             .where('hive_created_at', null);
+
+//         logger.info(
+//             `/services/hive/dates/updateHiveTxsWithNoDates: otherNullCreatedAts: ${otherNullCreatedAts?.length}, nullCreatedAts: ${nullCreatedAts?.length}`
+//         );
+//     } catch (err) {
+//         logger.error(
+//             `/services/hive/dates/updateHiveTxsWithNoDates error: ${err.message}`
+//         );
+//         throw err;
+//     }
+// };
 
 const getAndInsertNewHiveTxDates = async () => {
     try {
-        logger.debug(`/services/hive/dates/getAndInsertNewHiveTxDates`);
+        logger.info(`/services/hive/dates/getAndInsertNewHiveTxDates`);
         const notInsertedHiveTxIdsSql = `SELECT distinct ur.sell_trx_hive_id FROM user_rentals ur WHERE NOT EXISTS (SELECT hd.hive_tx_id FROM hive_tx_date hd WHERE ur.sell_trx_hive_id = hd.hive_tx_id)`;
         const unique_sell_hive_ids = await knexInstance.raw(
             notInsertedHiveTxIdsSql
         );
+
+        if (
+            !Array.isArray(unique_sell_hive_ids.rows) ||
+            unique_sell_hive_ids.rows?.length === 0
+        ) {
+            logger.info(
+                `/services/hive/dates/getAndInsertNewHiveTxDates no new hiveTxDates`
+            );
+            return null;
+        }
+        let chunks = unique_sell_hive_ids.rows;
+        if (unique_sell_hive_ids.rows?.length > 1000) {
+            chunks = _.chunk(unique_sell_hive_ids.rows, 1000);
+        }
+
+        if (chunks.length === unique_sell_hive_ids.rows) {
+            chunks = [chunks];
+        }
         logger.info(
-            `res of the raw is: ${JSON.stringify(
-                Object.keys(unique_sell_hive_ids)
-            )}, unique_sell_hive_ids: ${JSON.stringify(
-                unique_sell_hive_ids?.rows?.length
-            )} `
+            `/services/hive/dates/getAndInsertNewHiveTxDates chunks.length: ${chunks?.length}`
         );
-        logger.info(
-            `res of raw[0] is: ${JSON.stringify(unique_sell_hive_ids.rows[0])}`
-        );
-        // logger.info(
-        //     `res of the raw is: ${JSON.stringify(unique_sell_hive_ids)}`
-        // );
+        for (const idChunk of chunks) {
+            const txsWithDateToInsert = await getCreatedDatesForIds({
+                sell_trx_hive_ids: idChunk,
+            });
 
-        const txsWithDateToInsert = await getCreatedDatesForIds({
-            sell_trx_hive_ids: unique_sell_hive_ids.rows,
-        });
+            await insertHiveTxDates({ dateObjArr: txsWithDateToInsert });
+        }
+        // const txsWithDateToInsert = await getCreatedDatesForIds({
+        //     sell_trx_hive_ids: unique_sell_hive_ids.rows,
+        // });
 
-        await insertHiveTxDates({ dateObjArr: txsWithDateToInsert });
-
+        // await insertHiveTxDates({ dateObjArr: txsWithDateToInsert });
+        logger.info(`/services/hive/dates/getAndInsertNewHiveTxDates:`);
         return;
     } catch (err) {
         logger.error(
@@ -94,18 +130,18 @@ const insertHiveTxDates = async ({ dateObjArr }) => {
             return null;
         }
 
-        let chunks = dateObjArr;
-        if (dateObjArr?.length > 1000) {
-            chunks = _.chunk(dateObjArr, 1000);
-        }
+        // let chunks = dateObjArr;
+        // if (dateObjArr?.length > 1000) {
+        //     chunks = _.chunk(dateObjArr, 1000);
+        // }
 
-        if (chunks.length === dateObjArr.length) {
-            chunks = [chunks];
-        }
-        for (const dateChunk of chunks) {
-            await HiveTxDate.query().insert(dateChunk);
-        }
-
+        // if (chunks.length === dateObjArr.length) {
+        //     chunks = [chunks];
+        // }
+        // for (const dateChunk of chunks) {
+        //     await HiveTxDate.query().insert(dateChunk);
+        // }
+        await HiveTxDate.query().insert(dateObjArr);
         logger.info(
             `/services/hive/dates/insertHiveTxDates: inserted: ${dateObjArr.length}`
         );
@@ -120,17 +156,29 @@ const insertHiveTxDates = async ({ dateObjArr }) => {
 
 const getCreatedDatesForIds = async ({ sell_trx_hive_ids }) => {
     try {
-        logger.debug(`/services/hive/dates/getCreatedDatesForIds`);
+        logger.info(`/services/hive/dates/getCreatedDatesForIds`);
         const createdDatesForIds = [];
+        let count = 0;
         for (const { sell_trx_hive_id } of sell_trx_hive_ids) {
-            logger.info(
-                `/services/hive/dates/getCreatedDatesForIds sell_trx_hive_id: ${JSON.stringify(
-                    sell_trx_hive_id
-                )}`
-            );
-            const { hive_created_at_date } =
-                await relistingsService.getHiveTransactionDate({
+            // logger.info(
+            //     `/services/hive/dates/getCreatedDatesForIds sell_trx_hive_id: ${JSON.stringify(
+            //         sell_trx_hive_id
+            //     )}`
+            // );
+            if (count % 100 === 0) {
+                logger.info(
+                    `/services/hive/dates/getCreatedDatesForIds count: ${count}`
+                );
+            }
+            const { hive_created_at_date } = await relistingsService
+                .getHiveTransactionDate({
                     transactionId: sell_trx_hive_id,
+                })
+                .catch((err) => {
+                    logger.error(
+                        `/services/hive/dates/getCreatedDatesForIds got dates ${createdDatesForIds?.length}, error: ${err.message}`
+                    );
+                    return createdDatesForIds;
                 });
 
             const hiveTxDateObj = {
@@ -138,6 +186,7 @@ const getCreatedDatesForIds = async ({ sell_trx_hive_ids }) => {
                 hive_created_at: hive_created_at_date,
             };
             createdDatesForIds.push(hiveTxDateObj);
+            count = count + 1;
         }
         logger.info(
             `/services/hive/dates/getCreatedDatesForIds: ${createdDatesForIds?.length}`
