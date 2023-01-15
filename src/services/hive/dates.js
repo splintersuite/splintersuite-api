50d2e801bfca8e813d6cf9a29e220e354ce69a47
hackinhukk <trevorthomas@mac.com>
2023-01-15 02:52:50 -0500

    cleaning up commented out stuff

--- src/services/hive/dates.js
+++ src/services/hive/dates.js
@@ -1,7 +1,5 @@
 'use strict';
 const logger = require('../../util/pinologger');
-const retryFncs = require('../../util/axios_retry/general');
-const UserRentals = require('../../models/UserRentals');
 const HiveTxDate = require(`../../models/HiveTxDate`);
 const knexInstance = require('../../../db/index');
 const relistingsService = require('./relistings');
@@ -9,31 +7,9 @@ const _ = require('lodash');
 const updateHiveTxDates = async () => {
     try {
         logger.debug(`/services/hive/dates/updateHiveTxDates`);
-        // first get all the HiveTxDates in existence, get all the unique HiveTxDates
-        // we need to get all of the distinct hiveTxIds from UserRentals
-        // and then see which ones don't exist in already in hiveTxDate, once we have this
-        // we then need to insert the hiveTxIds that aren't in the table, and then look up the ones that don't have a hive_created_at not null
-        const distinctHiveIds = await UserRentals.query()
-            //.select('sell_trx_hive_id') // this makes it so we only get the sell_trx_hive_id back
-            .distinct('sell_trx_hive_id')
-            .debug();
 
-        // https://stackoverflow.com/questions/4076098/how-to-select-rows-with-no-matching-entry-in-another-table
-        // make SQL query that is like this
-
-        // then once we have all of these IDs, we need to actually insert the non new ones into the hive_tx_date table
-        // once this is done, we can then query this table for all of the ones that have a hive_created_at of null,
-        // and update that shit
-
-        // THEN WE CAN CHANGE OUR rentalConfirmation shit to also include recording and updating all of the unique hive_trx_ids
-
-        // TNT TODO: we also need a function to go over the userRentals and get them their sell_tx
-        logger.info(
-            ` count: ${distinctHiveIds?.length}, firstRow: ${JSON.stringify(
-                distinctHiveIds[0]
-            )}`
-        );
         await getAndInsertNewHiveTxDates();
+
         return;
     } catch (err) {
         logger.error(
@@ -68,6 +44,8 @@ const updateHiveTxDates = async () => {
 const getAndInsertNewHiveTxDates = async () => {
     try {
         logger.info(`/services/hive/dates/getAndInsertNewHiveTxDates`);
+
+        // https://stackoverflow.com/questions/4076098/how-to-select-rows-with-no-matching-entry-in-another-table
         const notInsertedHiveTxIdsSql = `SELECT distinct ur.sell_trx_hive_id FROM user_rentals ur WHERE NOT EXISTS (SELECT hd.hive_tx_id FROM hive_tx_date hd WHERE ur.sell_trx_hive_id = hd.hive_tx_id)`;
         const unique_sell_hive_ids = await knexInstance.raw(
             notInsertedHiveTxIdsSql
@@ -90,26 +68,15 @@ const getAndInsertNewHiveTxDates = async () => {
         if (chunks.length === unique_sell_hive_ids.rows.length) {
             chunks = [chunks];
         }
-        logger.info(
-            `/services/hive/dates/getAndInsertNewHiveTxDates chunks.length: ${chunks?.length}`
-        );
+
         for (const idChunk of chunks) {
-            logger.info(
-                `idChunk: ${JSON.stringify(idChunk)}, chunks: ${JSON.stringify(
-                    chunks
-                )}`
-            );
             const txsWithDateToInsert = await getCreatedDatesForIds({
                 sell_trx_hive_ids: idChunk,
             });
 
             await insertHiveTxDates({ dateObjArr: txsWithDateToInsert });
         }
-        // const txsWithDateToInsert = await getCreatedDatesForIds({
-        //     sell_trx_hive_ids: unique_sell_hive_ids.rows,
-        // });
 
-        // await insertHiveTxDates({ dateObjArr: txsWithDateToInsert });
         logger.info(`/services/hive/dates/getAndInsertNewHiveTxDates:`);
         return;
     } catch (err) {
@@ -135,17 +102,6 @@ const insertHiveTxDates = async ({ dateObjArr }) => {
             return null;
         }
 
-        // let chunks = dateObjArr;
-        // if (dateObjArr?.length > 1000) {
-        //     chunks = _.chunk(dateObjArr, 1000);
-        // }
-
-        // if (chunks.length === dateObjArr.length) {
-        //     chunks = [chunks];
-        // }
-        // for (const dateChunk of chunks) {
-        //     await HiveTxDate.query().insert(dateChunk);
-        // }
         await HiveTxDate.query().insert(dateObjArr);
         logger.info(
             `/services/hive/dates/insertHiveTxDates: inserted: ${dateObjArr.length}`
@@ -165,11 +121,6 @@ const getCreatedDatesForIds = async ({ sell_trx_hive_ids }) => {
         const createdDatesForIds = [];
         let count = 0;
         for (const { sell_trx_hive_id } of sell_trx_hive_ids) {
-            // logger.info(
-            //     `/services/hive/dates/getCreatedDatesForIds sell_trx_hive_id: ${JSON.stringify(
-            //         sell_trx_hive_id
-            //     )}`
-            // );
             if (count % 100 === 0) {
                 logger.info(
                     `/services/hive/dates/getCreatedDatesForIds count: ${count}`
